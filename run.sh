be915b63ad924dc61b49c6022b809f7e467a243a
hessamhz <hessam.hashemi21@gmail.com>
2023-02-25 14:32:34 +0100

    (hotfix) removed the extra files and added req

--- run.sh
+++
@@ -1,8 +0,0 @@
-# Install packages listed in dev.txt
-pip install -r requirements/dev.txt
-
-# Install packages listed in common.txt
-pip install -r requirements/common.txt
-
-# Install packages listed in test.txt
-pip install -r requirements/test.txt
