7307aa1526084988083613dbadb3c2904b344e29
Fatih Temiz <tmzfatih@gmail.com>
2023-02-26 12:45:19 +0100

    cleaning

--- elnet/src/functions/Spectrum_First_Fit.py
+++ elnet/src/functions/Spectrum_First_Fit.py
@@ -7,16 +7,14 @@ def First_Fit(N, path, num_slots):
     for first_slot in range(slots_in_band - num_slots + 1):
 
         spectrum_is_free = True
-        
-        #######################################################################################
-        # the missing part is written by students
+
         
         for link in zip(path, path[1:]):
             
             if sum(N.edges[link]['spectrum_slots'][first_slot : first_slot + num_slots]) != 0:
                 spectrum_is_free = False
                 break
-        #######################################################################################
+   
                 
         if spectrum_is_free:
             return first_slot
--- elnet/src/functions/clear_spectrum.py
+++ elnet/src/functions/clear_spectrum.py
@@ -3,8 +3,8 @@
 #We are using 4THz optical band for each link and we choose each slot has 12.5 GHz; so it makes 320 channels 
 
 
-def clearSpectrum(G):
+def clearSpectrum(N,num_slot=320):
     num_slot=320
-    for link in G.edges:
-        G.edges[link]['spectral_occupation']= [0]*num_slot
+    for link in N.edges:
+        N.edges[link]['spectral_occupation']= [0]*num_slot
 
