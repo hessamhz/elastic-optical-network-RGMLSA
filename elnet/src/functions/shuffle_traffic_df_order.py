cce8c8c48968e63f4b6cdba52a8d650b6a5919cb
Fatih Temiz <tmzfatih@gmail.com>
2023-03-04 19:03:45 +0100

    shuffle_traffic_df_order

---
+++ elnet/src/functions/shuffle_traffic_df_order.py
@@ -0,0 +1,19 @@
+#The frac keyword argument specifies the fraction of rows to return in the random sample,
+#so frac=1 means to return all rows (in random order).
+
+import random
+import pandas as pd
+
+
+def shuffle_traffic_df_order(traffic:pd.DataFrame,shuffling_parameter:int):
+    
+    shuffled_traffic_list=[]
+    for i in range(shuffling_parameter):
+        shuffled_traffic = traffic.sample(frac=1, random_state=random.randint(0, 1000)).reset_index(drop=True)
+        shuffled_traffic_list.append(shuffled_traffic)
+    
+    
+    return shuffled_traffic_list 
+
+
+
