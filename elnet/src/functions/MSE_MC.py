431313655235c453df36196a507764362dc38631
Fatih Temiz <tmzfatih@gmail.com>
2023-03-04 19:21:43 +0100

    overall_traffic_distribution

--- elnet/src/functions/MSE_MC.py
+++ elnet/src/functions/MSE_MC.py
@@ -1,39 +1,27 @@
 import pandas as pd
 
 from elnet.src.classes import AdvDiGraph
-from elnet.src.functions import choose_MF, compute_k_paths, create_path_df
+from elnet.src.functions import compute_k_paths, create_path_df,shuffle_traffic_df_order
 
 
 # MSE = Most Spectral Efficient
 # MC = Maximum Capacity
-def MSE_MC(
-    G: AdvDiGraph,
-    traffic: pd.DataFrame,
-    transponders: pd.DataFrame,
-    k_shortest_path=3,
-) -> None:
+def MSE_MC(G: AdvDiGraph, traffic: pd.DataFrame, k_shortest_path=3) -> None:
     # Making k shortest path dataframe
     path_dict = compute_k_paths(G, k_shortest_path)
     k_shortest_path_df = create_path_df(G, path_dict)
 
-    # Creating the joined tables of traffic and k_shortest_path for being more optimized
-    merged_traffic = pd.merge(
-        traffic, k_shortest_path_df, on=["src", "dst"], how="inner"
-    )
+    shuffled_traffic_list=shuffle_traffic_df_order(traffic)
+    overall_traffic_distribution=[]
 
-<<<<<<< HEAD
+    for i in range((len(shuffled_traffic_list))):
+        # Creating the joined tables of traffic and k_shortest_path for being more optimized
+        merged_traffic = pd.merge(
+        shuffled_traffic_list[i], k_shortest_path_df, on=["src", "dst"], how="inner")
+
+        overall_traffic_distribution.append(merged_traffic)
+
+
     
 
-    return merged_traffic
-=======
-    print(merged_traffic)
-    print(merged_traffic.iloc[0]["paths"])
-
-    MFs = []
-    # This is a bad practice to do iterrows
-    for index, row in merged_traffic.iterrows():
-        MFs.append(choose_MF(G, transponders, row["paths"]))
-    print(merged_traffic.iloc[0]["paths"])
-    print(MFs[0])
-    return
->>>>>>> 589795ceaa6d0c87e8ed1fc1d98c25cd9d744895
+    return overall_traffic_distribution
